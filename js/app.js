983a9123a1e2063ac9c6b4e19d1cb5986f6b76eb
Braden Amorozo <braden808@gmail.com>
2017-03-15 09:11:42 -1000

    update

---
+++ js/app.js
@@ -0,0 +1,58 @@
+console.log(sa)
+// var jsonDataBody = [
+// 	{
+// 		"header" : "center"  
+// 	},
+// 		"div" : {
+// 			"class" : "aside",
+// 			["img", 
+// 			]
+// 		}
+// ];
+
+// var jsonEntry = [
+// 	div : {
+// 		"id" : "first",
+// 		"class" : "content"
+// 	},
+
+// 	{
+// 		"h2"
+// 	},
+// 	{
+// 		"image" : "class"
+// 	}
+
+// ];
+
+var simplifiedJSON = [
+ {
+ 	"journalEntry" : "1",
+ 	"date" : "August 15, 2016",
+ 	"title" : "purpose"
+ 	"content" : "I took this class because I wanted to learn more about computer coding because I want to be a computer engineer I want to learn the most I can learn about coding.  I believe this is a very fun class that I can learn and perform in.",
+ 	"img" : "https://avatars1.githubusercontent.com/u/20955344?v=3&u=7d34617cc3904afbb2c2680b8fa1189b7df16360&s=140"
+ },
+ {
+ 	"journalEntry" : "2",
+ 	"date" : 
+ }
+]; 
+
+var dress = [
+	{
+		"title" : "mint",
+		"color scheme" : "mint and black",
+		"img" : "dresses",
+	}
+	{
+		"piechart" : ["Yes or no Data"],
+
+	}
+	{
+		"title": "top 10",
+		"div" {
+			[{"button" : "dress 1"}, {"button" "dress 2"}]
+		}
+	}
+];
